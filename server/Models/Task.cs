4193a43ca5761a1112c887d5ff70f9e0526bd303
MiriamR10 <rmg24895@gmail.com>
2026-03-18 11:07:43 +0200

    Update Task.cs

--- server/Models/Task.cs
+++ server/Models/Task.cs
@@ -2,7 +2,7 @@ namespace TaskManagerServer.Models;
 
 public class Task
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public string Priority { get; set; } = "נמוכה";
